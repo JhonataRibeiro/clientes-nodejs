9f62f6d7b9ee4ac35268d4aa2cd63a81041a023a
jhonataribeiro <jhonata.ribeiro.ti@gmail.com>
2019-06-11 20:43:58 -0300

    cria um doc

--- server.js
+++ server.js
@@ -3,18 +3,22 @@ const mongodb = require('mongodb');
 const config = require('./config/db');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-setTimeout(() => {
-  mongoose.connect(config.DB, {
-    useNewUrlParser: true
-  });
-}, 2000);
+mongoose.connect(config.DB, {
+  useNewUrlParser: true
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/v1', router);
 app.use('/', require('./routes/index.js'));
 
 app.listen(port, () => {
