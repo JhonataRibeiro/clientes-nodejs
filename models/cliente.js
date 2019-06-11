3c177ed423ab029148bf215c85d7486b4316321c
jhonataribeiro <jhonata.ribeiro.ti@gmail.com>
2019-06-11 20:56:37 -0300

    adiciona relacionamento

--- models/cliente.js
+++ models/cliente.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { agencia } = require('../models/agencia');
 
 const clienteSchema = new Schema({
   nome: { type: String, default: '' },
   email: { type: String, default: '' },
   cep: { type: String, default: '' },
   bairro: { type: String, default: '' },
-  agencia: { type: Number },
-  conta: { type: Number }
+  agencia: [{ type: Schema.Types.ObjectId, ref: 'Agencia' }]
 });
 
 const Cliente = mongoose.model('Cliente', clienteSchema);
