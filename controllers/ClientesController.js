204312374f45b51835b046e1d3277e55a19b5196
Jhonata Ribeiro <jhonatar@stj.jus.br>
2019-06-11 17:26:48 -0300

    remove metodo comentado

--- controllers/ClientesController.js
+++ controllers/ClientesController.js
@@ -19,18 +19,6 @@ const post = async (req, res) => {
   }
 };
 
-// exports.post = (req, res, next) => {
-//   try {
-//     var cliente = new Cliente(req.body);
-//     cliente.save(function(err, clienteSalvo) {
-//       if (err) return handleError(err);
-//       res.status(201).send({ cliente: clienteSalvo });
-//     });
-//   } catch (err) {
-//     console.log('error: ', err);
-//   }
-// };
-
 exports.put = (req, res, next) => {
   let id = req.params.id;
   res.status(201).send(`Requisição recebida com sucesso! ${id}`);
